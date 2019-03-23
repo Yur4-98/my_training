add7555ae5a9430e5a27663e4b80dd4f03626140
Yur4-98 <yura-kasyan@mail.ru>
2019-03-24 00:59:14 +1000

    Summary

---
+++ My first game/Assets/skripts/MoneyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyController : MonoBehaviour
+{
+    public GameObject TextObject;
+    Text textComponent;
+    public int coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = TextObject.GetComponent<Text>();
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "coin")
+        {
+            coin = coin + 1;
+            textComponent.text = coin.ToString() + "/9";
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+
--- My first game/Assets/skripts/characterController.cs
+++ My first game/Assets/skripts/characterController.cs
@@ -4,39 +4,47 @@ using System.Collections.Generic;
 
 public class characterController : MonoBehaviour {
 	public float maxSpeed = 10f;
-	public float jumpForce = 700f;
-	bool facingRight = true;
-	bool grounded = false;
-	public Transform groundCheck;
-	public float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public float score;
-	public float move;
+    public float jumpForce = 230f;
+    bool facingRight = true;
+    bool grounded = false;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float score;
+    public float move;
+    private Animator anim;
 
-	private GameObject star;
+    private GameObject star;
 	// Use this for initialization
-	void Start () {
-
-	}
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-
-		move = Input.GetAxis ("Horizontal");
-
-	}
+	void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+        if (!grounded)
+            return;
+        move = Input.GetAxis ("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
 
 	void Update(){
-		if (grounded && (Input.GetKeyDown (KeyCode.W)||Input.GetKeyDown (KeyCode.UpArrow))) {
 
-			GetComponent<Rigidbody2D>().AddForce (new Vector2(0f,jumpForce));
-		}
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		
-		if (move > 0 && !facingRight)
+        if (grounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+        }
+
+
+
+        if (move > 0 && !facingRight)
 			Flip ();
 		else if (move < 0 && facingRight)
 			Flip ();
